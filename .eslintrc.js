4ae7ebd0ebcadca98a4b33a749d12a6252cc307e
Mojtaba Namazi Ghamsari <mojtaba.namazi.ghamsari@gmail.com>
2023-09-30 22:26:31 +0330

    Add prettier config file and configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,32 @@
 module.exports = {
   root: true,
   extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
+    '@remix-run/eslint-config/jest-testing-library',
+    'prettier',
   ],
   globals: {
-    shopify: "readonly"
+    shopify: 'readonly',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          ['builtin', 'external'],
+          ['index', 'sibling', 'parent', 'internal', 'object'],
+        ],
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: '~/**',
+            group: 'internal',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+      },
+    ],
   },
 };
